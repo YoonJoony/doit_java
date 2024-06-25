2893d274b8a774093216c9e314cc4ba4d3649c73
YoonJoony <dbswns1101@gmail.com>
2024-06-25 10:33:55 +0900

    HashSet ex02

--- javadv/13day_HashSet/sec02_set/EX02_HashSetMachanism2/HashSetMachanism2.java
+++ javadv/13day_HashSet/sec02_set/EX02_HashSetMachanism2/HashSetMachanism2.java
@@ -1,16 +1,10 @@
-package sec02_set.EX02_HashSetMachanism;
+package sec02_set.EX02_HashSetMachanism2;
 
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-import javax.net.ssl.SSLContext;
-
-import sec02_set.EX02_HashSetMachanism.A;
-import sec02_set.EX02_HashSetMachanism.B;
-import sec02_set.EX02_HashSetMachanism.C;
-
-class A {
+class A{
 	int data;
 	public A(int data) {
 		this.data = data;
@@ -19,19 +13,20 @@ class A {
 
 class B {
 	int data;
+	
 	public B(int data) {
 		this.data = data;
 	}
+
 	
 	@Override
 	public boolean equals(Object obj) {
 		if(obj instanceof B) {
-			this.data = ((B)obj).data;
+			this.data = ((B) obj).data;
 			return true;
 		}
 		return false;
 	}
-	
 }
 
 class C {
@@ -43,7 +38,7 @@ class C {
 	@Override
 	public boolean equals(Object obj) {
 		if(obj instanceof C) {
-			this.data = ((C)obj).data;
+			this.data = ((C) obj).data;
 			return true;
 		}
 		return false;
@@ -53,33 +48,29 @@ class C {
 	public int hashCode() {
 		return Objects.hash(data);
 	}
+	
 }
 
-
-public class HashSetMachanism {
+public class HashSetMachanism2 {
 	public static void main(String[] args) {
 		// 1. 어떤것도 오버라이딩 하지 않음
 		Set<A> hashSet1 = new HashSet<>();
 		A a1 = new A(3);
 		A a2 = new A(3);
-		System.out.println(a1==a2);
-		System.out.println(a1.equals(a2));
+		System.out.println(a1==a2); //false
+		System.out.println(a1.equals(a2)); //false
 		System.out.println(a1.hashCode() + ", " + a2.hashCode());
-		hashSet1.add(a1);
-		hashSet1.add(a2);
-		System.out.println(hashSet1.size());
-		System.out.println();
 		
-		// 2. equals 메소드만 오버라이딩
+		//#2. equals 메서드만 오버라이딩
 		Set<B> hashSet2 = new HashSet<>();
 		B b1 = new B(3);
 		B b2 = new B(3);
-		System.out.println(b1==b2);
-		System.out.println(b1.equals(b2));
+		System.out.println(b1==b2); //false
+		System.out.println(b1.equals(b2)); //true
 		System.out.println(b1.hashCode() + ", " + b2.hashCode());
 		hashSet2.add(b1);
 		hashSet2.add(b2);
-		System.out.println(hashSet2.size());
+		System.out.println(hashSet2.size()); //2
 		System.out.println();
 		
 		//#2. equals + hashCode 메서드 오버라이딩
